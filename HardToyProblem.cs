384a80cf671c1f7fb8cf9f503b79c9a8aaf746a4
Shane Darcy <sdarcy@redventures.net>
2017-10-12 00:01:57 -0400

    started hard problem

---
+++ HardToyProblem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace HardToyProblem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char star = '*';
+            int numberOfRows = 6;
+            StringBuilder builder = new StringBuilder();
+
+            string[] starArray = new string[numberOfRows];
+            for (var i = 0; i <= numberOfRows; i++)
+            {
+                var stars = builder.Append(i).Append(star, 2);
+                
+                
+                Console.WriteLine(stars);
+                
+            }
+        }
+    }
+}
+
